687f903654a23a7dbea2d5ff759757feb19903af
TenViki <viktorkomarek28@gmail.com>
2022-08-13 17:50:18 +0200

    :lipstick: Designs card

---
+++ set-web/src/assets/shapes/Shapes.tsx
@@ -0,0 +1,6 @@
+export { ReactComponent as OvalEmpty } from "./empty oval.svg";
+export { ReactComponent as OvalDashed } from "./half oval.svg";
+export { ReactComponent as WaveEmpty } from "./empty wave.svg";
+export { ReactComponent as WaveDashed } from "./half wave.svg";
+export { ReactComponent as RhombusEmpty } from "./empty rhombus.svg";
+export { ReactComponent as RhombusDashed } from "./half rhombus.svg";
--- set-web/src/components/card-renderer/CardRenderer.tsx
+++ set-web/src/components/card-renderer/CardRenderer.tsx
@@ -1,21 +1,41 @@
 import React from "react";
 import { CardProps } from "../../types/CardType";
 import "./CardRenderer.scss";
+import {
+  OvalDashed,
+  OvalEmpty,
+  RhombusDashed,
+  RhombusEmpty,
+  WaveDashed,
+  WaveEmpty,
+} from "../../assets/shapes/Shapes";
 
 interface CardRendererProps {
   props: CardProps;
 }
 
 const CardRenderer: React.FC<CardRendererProps> = ({ props }) => {
+  let SelectedShape: any;
+
+  switch (props.shape) {
+    case 1:
+      SelectedShape = props.fill == 2 ? OvalDashed : OvalEmpty;
+      break;
+    case 2:
+      SelectedShape = props.fill == 2 ? WaveDashed : WaveEmpty;
+      break;
+    case 3:
+      SelectedShape = props.fill == 2 ? RhombusDashed : RhombusEmpty;
+      break;
+  }
+
+  if (!SelectedShape) return <div className="card"></div>;
+
   return (
-    <div className="card">
-      Color: {props.color}
-      <br />
-      Shape: {props.shape}
-      <br />
-      Count: {props.count}
-      <br />
-      Fill: {props.fill}
+    <div className={`card color-${props.color} fill-${props.fill}`}>
+      {[...Array(props.count)].map(() => (
+        <SelectedShape />
+      ))}
     </div>
   );
 };
