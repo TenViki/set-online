cbd2aebfea5dfd69e87bfd88d6cb987c68e48d52
TenViki <viktorkomarek28@gmail.com>
2022-08-29 11:27:55 +0200

    :wrench: Adds color prop to checkbox

--- set-web/src/components/fields/Checkbox.tsx
+++ set-web/src/components/fields/Checkbox.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { ColorType } from "../../types/color";
 import "./Checkbox.scss";
 
 interface CheckboxProps {
   checked: boolean;
   onChange: (checked: boolean) => void;
   label?: string;
+  color?: ColorType;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, label }) => {
+const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, label, color }) => {
   return (
-    <div className={`checkbox ${checked ? "active" : ""}`} onClick={() => onChange(!checked)}>
+    <div className={`checkbox ${checked ? "active" : ""} ${color ? color : "main"}`} onClick={() => onChange(!checked)}>
       <div className={`checkbox-squere`}>
         <span className="stick a" />
         <span className="stick b" />
