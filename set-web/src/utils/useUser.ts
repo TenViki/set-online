85b40cff2e169d59a58b8491a4d0f33c2cdcff91
TenViki <viktorkomarek28@gmail.com>
2022-08-29 21:34:07 +0200

    :fire: Removes debug log

--- set-web/src/utils/useUser.ts
+++ set-web/src/utils/useUser.ts
@@ -3,7 +3,6 @@ import { UserContext } from "../App";
 
 export const useUser = () => {
   const user = useContext(UserContext);
-  console.log(user);
   const usr = {
     ...(user.user || {}),
     isLoggedIn: !!user.user,
