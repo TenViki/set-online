65c5b6e12edc991ffc507835b91ae171d006d08e
TenViki <viktorkomarek28@gmail.com>
2022-09-07 20:27:34 +0200

    :zap: Upgrades game control panel

--- set-web/src/pages/game/components/GameControls.tsx
+++ set-web/src/pages/game/components/GameControls.tsx
@@ -1,21 +1,46 @@
-import React from "react";
-import { FiChevronRight, FiCopy, FiLink, FiUserPlus } from "react-icons/fi";
+import React, { FC } from "react";
+import { FiChevronRight, FiCopy, FiLink, FiLogOut, FiUserPlus } from "react-icons/fi";
 import Button from "../../../components/button/Button";
+import { GameType } from "../../../types/Game.type";
+import { useGame } from "../../../utils/useGame";
+import { useUser } from "../../../utils/useUser";
 import "./GameControls.scss";
 
 const GameControls = () => {
-  return (
-    <div className="lobby-game-controls">
-      <div className="lobby-game-controls-buttons">
-        <Button color="gray" text="Invite players" leftIcon={FiUserPlus} />
-        <Button color="gray" text="Copy game code" leftIcon={FiCopy} />
-        <Button color="gray" text="Copy join link" leftIcon={FiLink} />
+  const game = useGame();
+  const user = useUser();
+
+  if (user.isLoggedIn && game.game?.host.id === user.id)
+    return (
+      <div className="lobby-game-controls">
+        <div className="lobby-game-controls-buttons">
+          <Button color="gray" text="Invite players" leftIcon={FiUserPlus} />
+          <Button color="gray" text="Copy join link" leftIcon={FiLink} />
+          <Button color="gray" text="Copy game code" leftIcon={FiCopy} />
+          <div className="lobby-game-code text">
+            Game code:
+            <span>{game.game?.code}</span>
+          </div>
+        </div>
+        <div className="lobby-game-controls-start">
+          <Button color="success" text="Start game" rightIcon={FiChevronRight} />
+        </div>
       </div>
-      <div className="lobby-game-controls-start">
-        <Button color="success" text="Start game" rightIcon={FiChevronRight} />
+    );
+  else {
+    return (
+      <div className="lobby-game-controls">
+        <div className="lobby-game-controls-buttons">
+          <Button color="danger" text="Leave" leftIcon={FiLogOut} />
+          <div className="lobby-game-code text">
+            Game code:
+            <span>{game.game?.code}</span>
+          </div>
+        </div>
+        <div className="lobby-game-controls-waiting">Waiting for host to start...</div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default GameControls;
